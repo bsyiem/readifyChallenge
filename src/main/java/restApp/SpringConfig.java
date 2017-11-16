ccf1979453065f9b09c9de10baa6c9176b2ad066
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-16 20:08:58 +1100

    set content type to application/json

---
+++ src/main/java/restApp/SpringConfig.java
@@ -0,0 +1,22 @@
+package restApp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by brandon on 16/11/17.
+ */
+
+@Configuration
+public class SpringConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        AntPathMatcher matcher = new AntPathMatcher();
+        matcher.setCaseSensitive(false);
+        configurer.setPathMatcher(matcher);
+    }
+}
--- src/main/java/restApp/controller/TokenController.java
+++ src/main/java/restApp/controller/TokenController.java
@@ -17,7 +17,7 @@ public class TokenController {
 
     private String token = "\"fe2c780f-4bc3-4759-b5ed-0f363d64b11d\"";
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/{([tT][oO][kK][eE][nN]) }", produces = {"application/json", "text/json"})
+    @RequestMapping(method = RequestMethod.GET, value = "/api/token", produces = {"application/json", "text/json"})
     public ResponseEntity<String> sendToken(){
 
         HttpHeaders responseHeaders = new HttpHeaders();
