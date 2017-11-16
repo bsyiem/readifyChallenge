253d968f4de799f88f972a1cd44d6e260c130bba
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-16 20:30:09 +1100

    set content type to application/json

--- src/main/java/restApp/controller/ErrorController.java
+++ src/main/java/restApp/controller/ErrorController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -18,8 +19,7 @@ import restApp.entities.ApiError;
 @ControllerAdvice
 public class ErrorController {
 
-    @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @RequestMapping(value = "/error")
     public ResponseEntity<ApiError> sendError(){
 
         HttpHeaders responseHeaders = new HttpHeaders();
