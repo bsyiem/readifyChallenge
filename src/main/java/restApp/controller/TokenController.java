15003aa533ad7f425b375e3b9b3d46098d14f628
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-16 17:49:44 +1100

    set content type to application/json

--- src/main/java/restApp/controller/TokenController.java
+++ src/main/java/restApp/controller/TokenController.java
@@ -13,8 +13,11 @@ import java.lang.reflect.Method;
 @RestController
 public class TokenController {
 
+    private String token = "fe2c780f-4bc3-4759-b5ed-0f363d64b11d";
+
     @RequestMapping(method = RequestMethod.GET, value = "/api/token", produces = {"application/json"})
     public ResponseEntity<String> sendToken(){
-        return ResponseEntity.ok("fe2c780f-4bc3-4759-b5ed-0f363d64b11d");
+
+        return ResponseEntity.ok(token);
     }
 }
