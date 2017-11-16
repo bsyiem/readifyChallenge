2dced86f8dccbce91a1eff6bf8814c91cf37aadf
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-16 18:21:14 +1100

    set content type to application/json

--- src/main/java/restApp/controller/TokenController.java
+++ src/main/java/restApp/controller/TokenController.java
@@ -4,7 +4,6 @@ package restApp.controller;
  * Created by brandon on 16/11/17.
  * Controller for sending valid token
  */
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +14,7 @@ public class TokenController {
 
     private String token = "\"fe2c780f-4bc3-4759-b5ed-0f363d64b11d\"";
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/token", produces = {"application/json"})
+    @RequestMapping(method = RequestMethod.GET, value = "/api/{[T|t][O|o][K|k][E|e][N|n]}", produces = {"application/json"})
     public ResponseEntity<String> sendToken(){
 
         return ResponseEntity.ok(token);
