191dd1b848719a97741d0df92a11a211e8c38deb
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-16 17:09:13 +1100

    change to only respond to GET requests

--- src/main/java/restApp/controller/TokenController.java
+++ src/main/java/restApp/controller/TokenController.java
@@ -6,10 +6,12 @@ package restApp.controller;
  */
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Method;
+
 @RestController
 public class TokenController {
 
-    @RequestMapping("/api/token")
+    @RequestMapping(method = RequestMethod.GET, path = "/api/token")
     public String sendToken(){
         return "fe2c780f-4bc3-4759-b5ed-0f363d64b11d";
     }
