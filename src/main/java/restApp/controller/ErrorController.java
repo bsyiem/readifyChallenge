019284d2ae3297ec0cf257d3ef7cd8bf573055ce
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-16 20:48:56 +1100

    set content type to application/json

--- src/main/java/restApp/controller/ErrorController.java
+++ src/main/java/restApp/controller/ErrorController.java
@@ -1,27 +1,27 @@
-//package restApp.controller;
-//
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import restApp.entities.ApiError;
-//
-///**
-// * Created by brandon on 16/11/17.
-// */
-//@RestController
-//public class ErrorController {
-//
-//    @RequestMapping(value = "/error")
-//    public ResponseEntity<ApiError> sendError(){
-//
-//        HttpHeaders responseHeaders = new HttpHeaders();
-//        responseHeaders.setCacheControl("no-cache");
-//        responseHeaders.setExpires(-1);
-//        responseHeaders.setPragma("no-cache");
-//
-//        ApiError error = new ApiError("error: 404");
-//        return new ResponseEntity<ApiError>(error,responseHeaders,HttpStatus.OK);
-//    }
-//
-//}
+package restApp.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import restApp.entities.ApiError;
+
+/**
+ * Created by brandon on 16/11/17.
+ */
+@RestController
+public class ErrorController {
+
+    @RequestMapping(value = "/error")
+    public ResponseEntity<ApiError> sendError(){
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setCacheControl("no-cache");
+        responseHeaders.setExpires(-1);
+        responseHeaders.setPragma("no-cache");
+
+        ApiError error = new ApiError("error: 404");
+        return new ResponseEntity<ApiError>(error,responseHeaders,HttpStatus.NOT_FOUND);
+    }
+
+}
