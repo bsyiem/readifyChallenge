e3139152a23835bb06ffb5c14cb756e31692bd7e
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-16 17:30:53 +1100

    fixed response http status

--- src/main/java/restApp/controller/TokenController.java
+++ src/main/java/restApp/controller/TokenController.java
@@ -4,6 +4,8 @@ package restApp.controller;
  * Created by brandon on 16/11/17.
  * Controller for sending valid token
  */
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Method;
@@ -12,7 +14,7 @@ import java.lang.reflect.Method;
 public class TokenController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/api/token")
-    public String sendToken(){
-        return "fe2c780f-4bc3-4759-b5ed-0f363d64b11d";
+    public ResponseEntity<String> sendToken(){
+        return ResponseEntity.ok("fe2c780f-4bc3-4759-b5ed-0f363d64b11d");
     }
 }
