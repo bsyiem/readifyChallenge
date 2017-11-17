5ebcf4dbad2340913449742a38e677344d0ed192
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-17 23:43:51 +1100

    set content type to application/json

---
+++ src/test/java/restApp/Logic/TriangleCheckTest.java
@@ -0,0 +1,35 @@
+package restApp.Logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by brandon on 17/11/17.
+ */
+public class TriangleCheckTest {
+    @Test
+    public void checkTriangle() throws Exception {
+        int a1 = 1;
+        int b1 = 2;
+        int c1 = 3;
+
+        int a2 = 2;
+        int b2 = 2;
+        int c2 = 3;
+
+        int a3 = 2;
+        int b3 = 2;
+        int c3 = 2;
+
+        int a4 = 2;
+        int b4 = 2;
+        int c4 = 5;
+
+        assertEquals(TriangleCheck.checkTriangle(a1,b1,c1),"scalene");
+        assertEquals(TriangleCheck.checkTriangle(a2,b2,c2),"isosceles");
+        assertEquals(TriangleCheck.checkTriangle(a3,b3,c3),"equilateral");
+        assertEquals(TriangleCheck.checkTriangle(a4,b4,c4),"error");
+    }
+
+}
