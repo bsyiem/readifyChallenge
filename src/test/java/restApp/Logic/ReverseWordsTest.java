9cf300e1b64f8aea88bf13c87a65f6e508e76880
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-18 00:19:23 +1100

    set content type to application/json

--- src/test/java/restApp/Logic/ReverseWordsTest.java
+++ src/test/java/restApp/Logic/ReverseWordsTest.java
@@ -14,7 +14,11 @@ public class ReverseWordsTest {
         String word = "abc 123";
         String reverse = "cba 321";
 
+        String word1 = "abc";
+        String reverse1 = "cba";
+
         assertEquals(ReverseWords.reverse(word),reverse);
+        assertEquals(ReverseWords.reverse(word1),reverse1);
     }
 
 }
