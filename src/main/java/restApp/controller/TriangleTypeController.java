dcda777e12dc088d6a03ef74220aa9776d4ab7fc
Brandon <bsyiem@student.unimelb.edu.au>
2017-11-17 23:23:39 +1100

    set content type to application/json

--- src/main/java/restApp/controller/TriangleTypeController.java
+++ src/main/java/restApp/controller/TriangleTypeController.java
@@ -11,7 +11,7 @@ import restApp.Logic.TriangleCheck;
 
 @RestController
 public class TriangleTypeController {
-    @RequestMapping(value = "/api/triagleType", produces = {"application/json","text/json"})
+    @RequestMapping(value = "/api/triangleType", produces = {"application/json","text/json"})
     public String sendTriangleType(@RequestParam(name = "a",required = true) int a, @RequestParam(name = "a",required = true) int b, @RequestParam(name = "a",required = true) int c){
         return TriangleCheck.checkTriangle(a,b,c);
     }
